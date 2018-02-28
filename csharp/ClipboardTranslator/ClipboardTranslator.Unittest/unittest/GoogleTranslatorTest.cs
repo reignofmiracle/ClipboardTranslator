f4f2c7d4d2a2b460c372036d14b64df9576da638
reign <reignofmiracle@gmail.com>
2018-02-28 19:08:00 +0900

    update translator

---
+++ csharp/ClipboardTranslator/ClipboardTranslator.Unittest/unittest/GoogleTranslatorTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+
+namespace ClipboardTranslator.Unittest.unittest
+{
+    [TestFixture]
+    public class GoogleTranslatorTest
+    {
+        [Test]
+        public void Translate()
+        {
+            var testObj = new GoogleTranslator();
+            var ret = testObj.Translate(@"There seems to be some controversy about the advantages of static vs.
+dynamic and strong vs. weak typing. Let me illustrate these choices
+with a thought experiment. Imagine millions of monkeys at computer
+keyboards happily hitting random keys, producing programs, compiling,
+and running them.", "en", "ko");
+            Debug.WriteLine(ret);
+        }
+    }
+}
--- csharp/ClipboardTranslator/ClipboardTranslator/GoogleTranslator.cs
+++ csharp/ClipboardTranslator/ClipboardTranslator/GoogleTranslator.cs
@@ -8,19 +8,25 @@ namespace ClipboardTranslator
     public class GoogleTranslator
     {
         private WebClient webClient = new WebClient();
-        private List<string> filterList = new List<string> { "\\r", "\\x3cbr\\x3e", "\\x26quot;"};
 
         public string Translate(string text, string translateFrom, string translateTo)
         {
             try
             {
-                string url = String.Format("http://www.google.com/translate_t?hl=en&ie=UTF8&text={0}&langpair={1}", HttpUtility.UrlEncode(text), translateFrom + "|" + translateTo);
+                string url = String.Format("http://www.google.com/translate_t?hl={0}&ie=UTF8&text={1}&langpair={2}", translateFrom, HttpUtility.UrlEncode(text), translateFrom + "|" + translateTo);
                 string html = webClient.DownloadString(url);
                 int from = html.IndexOf("TRANSLATED_TEXT='") + "TRANSLATED_TEXT='".Length;
                 int to = html.Substring(from).IndexOf("';var");
                 var raw = html.Substring(from, to);
-                var result = WebUtility.HtmlDecode(raw);
-                this.filterList.ForEach(v => result = result.Replace(v, ""));
+                var result = raw
+                    .Replace(@"\r", "")
+                    .Replace(@"\xa0", " ")
+                    .Replace(@"\x3c", "<")
+                    .Replace(@"\x3e", ">")
+                    .Replace(@"\x26", "&")
+                    .Replace(@"\x22", "\"")
+                    .Replace(@"\x27", "'")
+                    .Replace("<br>", " ");
                 return result;
             }
             catch(Exception e)
