92fe2bef4b9fa60be05564013e4a318c917d79f3
reign <reignofmiracle@gmail.com>
2018-03-05 23:31:06 +0900

    working on menu

--- electron/app/index.js
+++ electron/app/index.js
@@ -1,133 +1,155 @@
-const {clipboard} = require('electron')
+const {clipboard, remote} = require('electron')
+const {Menu, MenuItem} = remote
 
-function prepare() {
-    languageList = [
-        ["Afrikaans","af"],
-        ["Albanian","sq"],
-        ["Amharic","am"],
-        ["Arabic","ar"],
-        ["Armenian","hy"],
-        ["Azeerbaijani","az"],
-        ["Basque","eu"],
-        ["Belarusian","be"],
-        ["Bengali","bn"],
-        ["Bosnian","bs"],
-        ["Bulgarian","bg"],
-        ["Catalan","ca"],
-        ["Cebuano","ceb"],
-        ["Chinese (Simplified)","zh-CN"],
-        ["Chinese (Traditional)","zh-TW"],
-        ["Corsican","co"],
-        ["Croatian","hr"],
-        ["Czech","cs"],
-        ["Danish","da"],
-        ["Dutch","nl"],
-        ["English","en"],
-        ["Esperanto","eo"],
-        ["Estonian","et"],
-        ["Finnish","fi"],
-        ["French","fr"],
-        ["Frisian","fy"],
-        ["Galician","gl"],
-        ["Georgian","ka"],
-        ["German","de"],
-        ["Greek","el"],
-        ["Gujarati","gu"],
-        ["Haitian Creole","ht"],
-        ["Hausa","ha"],
-        ["Hawaiian","haw"],
-        ["Hebrew","iw"],
-        ["Hindi","hi"],
-        ["Hmong","hmn (ISO-639-2)"],
-        ["Hungarian","hu"],
-        ["Icelandic","is"],
-        ["Igbo","ig"],
-        ["Indonesian","id"],
-        ["Irish","ga"],
-        ["Italian","it"],
-        ["Japanese","ja"],
-        ["Javanese","jw"],
-        ["Kannada","kn"],
-        ["Kazakh","kk"],
-        ["Khmer","km"],
-        ["Korean","ko"],
-        ["Kurdish","ku"],
-        ["Kyrgyz","ky"],
-        ["Lao","lo"],
-        ["Latin","la"],
-        ["Latvian","lv"],
-        ["Lithuanian","lt"],
-        ["Luxembourgish","lb"],
-        ["Macedonian","mk"],
-        ["Malagasy","mg"],
-        ["Malay","ms"],
-        ["Malayalam","ml"],
-        ["Maltese","mt"],
-        ["Maori","mi"],
-        ["Marathi","mr"],
-        ["Mongolian","mn"],
-        ["Myanmar (Burmese)","my"],
-        ["Nepali","ne"],
-        ["Norwegian","no"],
-        ["Nyanja (Chichewa)","ny"],
-        ["Pashto","ps"],
-        ["Persian","fa"],
-        ["Polish","pl"],
-        ["Portuguese (Portugal , Brazil)","pt"],
-        ["Punjabi","pa"],
-        ["Romanian","ro"],
-        ["Russian","ru"],
-        ["Samoan","sm"],
-        ["Scots Gaelic","gd"],
-        ["Serbian","sr"],
-        ["Sesotho","st"],
-        ["Shona","sn"],
-        ["Sindhi","sd"],
-        ["Sinhala (Sinhalese)","si"],
-        ["Slovak","sk"],
-        ["Slovenian","sl"],
-        ["Somali","so"],
-        ["Spanish","es"],
-        ["Sundanese","su"],
-        ["Swahili","sw"],
-        ["Swedish","sv"],
-        ["Tagalog (Filipino)","tl"],
-        ["Tajik","tg"],
-        ["Tamil","ta"],
-        ["Telugu","te"],
-        ["Thai","th"],
-        ["Turkish","tr"],
-        ["Ukrainian","uk"],
-        ["Urdu","ur"],
-        ["Uzbek","uz"],
-        ["Vietnamese","vi"],
-        ["Welsh","cy"],
-        ["Xhosa","xh"],
-        ["Yiddish","yi"],
-        ["Yoruba","yo"],
-        ["Zulu","zu"]]
-    
-    languageList.forEach((element, index, array) => {
-        option = document.createElement("option")
-        option.label = element[0]
-        option.value = element[1]
-        translateFrom.add(option)
-    })
-    translateFrom.selectedIndex = languageList.findIndex(elem => elem[1] == 'en')
+languageList = [
+    ["Afrikaans","af"],
+    ["Albanian","sq"],
+    ["Amharic","am"],
+    ["Arabic","ar"],
+    ["Armenian","hy"],
+    ["Azeerbaijani","az"],
+    ["Basque","eu"],
+    ["Belarusian","be"],
+    ["Bengali","bn"],
+    ["Bosnian","bs"],
+    ["Bulgarian","bg"],
+    ["Catalan","ca"],
+    ["Cebuano","ceb"],
+    ["Chinese (Simplified)","zh-CN"],
+    ["Chinese (Traditional)","zh-TW"],
+    ["Corsican","co"],
+    ["Croatian","hr"],
+    ["Czech","cs"],
+    ["Danish","da"],
+    ["Dutch","nl"],
+    ["English","en"],
+    ["Esperanto","eo"],
+    ["Estonian","et"],
+    ["Finnish","fi"],
+    ["French","fr"],
+    ["Frisian","fy"],
+    ["Galician","gl"],
+    ["Georgian","ka"],
+    ["German","de"],
+    ["Greek","el"],
+    ["Gujarati","gu"],
+    ["Haitian Creole","ht"],
+    ["Hausa","ha"],
+    ["Hawaiian","haw"],
+    ["Hebrew","iw"],
+    ["Hindi","hi"],
+    ["Hmong","hmn (ISO-639-2)"],
+    ["Hungarian","hu"],
+    ["Icelandic","is"],
+    ["Igbo","ig"],
+    ["Indonesian","id"],
+    ["Irish","ga"],
+    ["Italian","it"],
+    ["Japanese","ja"],
+    ["Javanese","jw"],
+    ["Kannada","kn"],
+    ["Kazakh","kk"],
+    ["Khmer","km"],
+    ["Korean","ko"],
+    ["Kurdish","ku"],
+    ["Kyrgyz","ky"],
+    ["Lao","lo"],
+    ["Latin","la"],
+    ["Latvian","lv"],
+    ["Lithuanian","lt"],
+    ["Luxembourgish","lb"],
+    ["Macedonian","mk"],
+    ["Malagasy","mg"],
+    ["Malay","ms"],
+    ["Malayalam","ml"],
+    ["Maltese","mt"],
+    ["Maori","mi"],
+    ["Marathi","mr"],
+    ["Mongolian","mn"],
+    ["Myanmar (Burmese)","my"],
+    ["Nepali","ne"],
+    ["Norwegian","no"],
+    ["Nyanja (Chichewa)","ny"],
+    ["Pashto","ps"],
+    ["Persian","fa"],
+    ["Polish","pl"],
+    ["Portuguese (Portugal , Brazil)","pt"],
+    ["Punjabi","pa"],
+    ["Romanian","ro"],
+    ["Russian","ru"],
+    ["Samoan","sm"],
+    ["Scots Gaelic","gd"],
+    ["Serbian","sr"],
+    ["Sesotho","st"],
+    ["Shona","sn"],
+    ["Sindhi","sd"],
+    ["Sinhala (Sinhalese)","si"],
+    ["Slovak","sk"],
+    ["Slovenian","sl"],
+    ["Somali","so"],
+    ["Spanish","es"],
+    ["Sundanese","su"],
+    ["Swahili","sw"],
+    ["Swedish","sv"],
+    ["Tagalog (Filipino)","tl"],
+    ["Tajik","tg"],
+    ["Tamil","ta"],
+    ["Telugu","te"],
+    ["Thai","th"],
+    ["Turkish","tr"],
+    ["Ukrainian","uk"],
+    ["Urdu","ur"],
+    ["Uzbek","uz"],
+    ["Vietnamese","vi"],
+    ["Welsh","cy"],
+    ["Xhosa","xh"],
+    ["Yiddish","yi"],
+    ["Yoruba","yo"],
+    ["Zulu","zu"]]
 
-    languageList.forEach((element, index, array) => {
-        option = document.createElement("option")
-        option.label = element[0]
-        option.value = element[1]
-        translateTo.add(option)
-    })
-    translateTo.selectedIndex = languageList.findIndex(elem => elem[1] == 'ko')
+languageList.forEach((element, index, array) => {
+    option = document.createElement("option")
+    option.label = element[0]
+    option.value = element[1]
+    translateFrom.add(option)
+})
+translateFrom.selectedIndex = languageList.findIndex(elem => elem[1] == 'en')
+
+languageList.forEach((element, index, array) => {
+    option = document.createElement("option")
+    option.label = element[0]
+    option.value = element[1]
+    translateTo.add(option)
+})
+translateTo.selectedIndex = languageList.findIndex(elem => elem[1] == 'ko')
+
+let settings = {
+    "newline_sentence" : false,
+    "fontSize" : '50px'
 }
 
-prepare()
+function adjustSettings(settings) {
+    resultTextArea.style.fontSize = settings.fontSize
+}
+
+adjustSettings(settings)
+
+const menu = new Menu()
+menu.append(new MenuItem({label: 'FontSize', submenu: [
+    new MenuItem({label: '10px', type: 'checkbox', click(menuItem, browserWindow) { }}),
+    new MenuItem({label: '15px', type: 'checkbox', click(menuItem, browserWindow) { }}),
+    new MenuItem({label: '20px', type: 'checkbox', click(menuItem, browserWindow) { }}),
+    new MenuItem({label: '25px', type: 'checkbox', click(menuItem, browserWindow) { }}),
+    new MenuItem({label: '30px', type: 'checkbox', click(menuItem, browserWindow) { }})
+]}))
+menu.append(new MenuItem({label: 'NewLine Sentence', type: 'checkbox', click(menuItem) { settings.newline_sentence = menuItem.checked }}))
+
+window.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    menu.popup(remote.getCurrentWindow())
+}, false)
 
 let query = 'http://www.google.com/translate_t?hl={0}&ie=UTF8&text={1}&langpair={2}'
-
 function translate(text, from, to, func) {
     url = query.replace('{0}', from).replace('{1}', encodeURI(text)).replace('{2}', from + "|" + to)
     console.log(url)
--- electron/main.js
+++ electron/main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow() {
     mainWindow = new BrowserWindow({width: 800, height: 600})
-    //mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'app/index.html'),
